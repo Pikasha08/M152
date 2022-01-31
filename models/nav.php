96d4f9b0c578090d1e054e11f72e4185a58c4d3c
Pikasha08 <79849695+Pikasha08@users.noreply.github.com>
2022-01-31 13:06:31 +0100

    Projet

---
+++ models/nav.php
@@ -0,0 +1,14 @@
+<nav>
+    <img src="https://brandlogos.net/wp-content/uploads/2021/02/bing-logo.png" alt="logo" width="25px">
+    <input type="text" name="" id="">
+    <button type="submit"><img src="https://cdn-icons-png.flaticon.com/512/694/694985.png" alt="loupe" width="25px"></button>
+
+    <a href="index.php" id="home">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e6/Home_Icon.svg/1200px-Home_Icon.svg.png" width="20px" alt="icon">
+        Home
+    </a>
+    <a href="post.php" id="post">
+        <img src="https://cdn0.iconfinder.com/data/icons/very-basic-2-android-l-lollipop-icon-pack/24/plus-512.png" width="20px" alt="icon">
+        Post
+    </a>
+</nav>
