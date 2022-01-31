ef905b3d4093747b9338d16358936cb5781eb380
Pikasha08 <79849695+Pikasha08@users.noreply.github.com>
2022-01-31 13:18:07 +0100

    Small update

--- index.php
+++ index.php
@@ -1,17 +1,225 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style/style.css">
-    <title>Home</title>
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+    <meta charset="utf-8">
+    <title>Portfolio</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <link href="style/bootstrap.css" rel="stylesheet">
+    <link href="style/facebook.css" rel="stylesheet">
 </head>
+
 <body>
-    <?php include 'models/nav.php'; ?>
-    <section>
-        <img src="img/Ussr.png" alt="PDP" height="150px">
-        <h1>Welcome</h1>
-    </section>
+
+    <div class="wrapper">
+        <!-- main right col -->
+        <div class="column col-sm-10 col-xs-11" id="main">
+            <!-- top nav -->
+            <div class="navbar navbar-blue navbar-static-top">
+                <div class="navbar-header">
+                    <button class="navbar-toggle" type="button" data-toggle="collapse" data-target=".navbar-collapse">
+                        <span class="sr-only">Toggle</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a href="#" class="navbar-brand logo">b</a>
+                </div>
+                <nav class="collapse navbar-collapse" role="navigation">
+                    <form class="navbar-form navbar-left">
+                        <div class="input-group input-group-sm" style="max-width:360px;">
+                            <input class="form-control" placeholder="Search" name="srch-term" id="srch-term" type="text">
+                            <div class="input-group-btn">
+                                <button class="btn btn-default" type="submit"></button>
+                            </div>
+                        </div>
+                    </form>
+                    <ul class="nav navbar-nav">
+                        <li>
+                            <a href="#">Home</a>
+                        </li>
+                        <li>
+                            <a href="post.php" role="button" data-toggle="modal">Post</a>
+                        </li>
+                        <li>
+                            <a href="#"><span class="badge">badge</span></a>
+                        </li>
+                    </ul>
+                    <ul class="nav navbar-nav navbar-right">
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown"></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="">More</a></li>
+                                <li><a href="">More</a></li>
+                                <li><a href="">More</a></li>
+                                <li><a href="">More</a></li>
+                                <li><a href="">More</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+            <!-- /top nav -->
+
+            <div class="padding">
+                <div class="full col-sm-9">
+                    <!-- content -->
+                    <div class="row">
+                        <!-- main col left -->
+                        <div class="col-sm-5">
+                            <div class="panel panel-default">
+                                <div class="panel-thumbnail"><img src="img/Ussr.png" class="img-responsive"></div>
+                                <div class="panel-body">
+                                    <p class="lead">Motherland</p>
+                                    <p>69 Followers, 1256 Posts</p>
+                                    <p>
+                                        <img src="img/uFp_tsTJboUY7kue5XAsGAs28.png" height="28px" width="28px">
+                                    </p>
+                                </div>
+                            </div>
+
+                            <div class="well">
+                                <form class="form-horizontal" role="form">
+                                    <h4>What's New</h4>
+                                    <div class="form-group" style="padding:14px;">
+                                        <textarea class="form-control" placeholder="Update your status"></textarea>
+                                    </div>
+                                    <button class="btn btn-primary pull-right" type="button">Post</button>
+                                    <ul class="list-inline">
+                                        <li><a href=""></a></li>
+                                    </ul>
+                                </form>
+                            </div>
+
+                            <div class="panel panel-default">
+                                <div class="panel-heading"><h4>Title</h4></div>
+                                <div class="panel-body">
+                                <p><img src="img/150x150.gif" class="img-circle pull-right"><a href="#">Image</a></p>
+                                <div class="clearfix"></div>
+                                <hr>
+                                Description
+                                </div>
+                            </div>
+
+                            <div class="panel panel-default">
+                                <div class="panel-heading"><h4>Title</h4></div>
+                                <div class="panel-body">
+                                <p><img src="img/150x150.gif" class="img-circle pull-right"><a href="#">Image</a></p>
+                                <div class="clearfix"></div>
+                                <hr>
+                                Description
+                                </div>
+                            </div>
+                        </div>
+
+                        <!-- main col right -->
+                        <div class="col-sm-7">
+
+                            <div class="panel panel-default">
+                                <div class="panel-heading">
+                                    <h4>Title</h4>
+                                </div>
+                                <div class="panel-body">
+                                    <p><img src="img/150x150.gif" class="img-circle pull-right"><a href="#">Image</a></p>
+                                    <div class="clearfix"></div>
+                                    <hr>
+                                    Description
+                                </div>
+                            </div>
+
+                            <div class="panel panel-default">
+                                <div class="panel-heading">
+                                    <h4>Title</h4>
+                                </div>
+                                <div class="panel-body">
+                                    <p><img src="img/150x150.gif" class="img-circle pull-right"><a href="#">Image</a></p>
+                                    <div class="clearfix"></div>
+                                    <hr>
+                                    Description
+                                </div>
+                            </div>
+
+                            <div class="panel panel-default">
+                                <div class="panel-heading">
+                                    <h4>Title</h4>
+                                </div>
+                                <div class="panel-body">
+                                    <p><img src="img/150x150.gif" class="img-circle pull-right"><a href="#">Image</a></p>
+                                    <div class="clearfix"></div>
+                                    <hr>
+                                    Description
+                                </div>
+                            </div>
+
+                            <div class="panel panel-default">
+                                <div class="panel-heading">
+                                    <h4>Title</h4>
+                                </div>
+                                <div class="panel-body">
+                                    <p><img src="img/150x150.gif" class="img-circle pull-right"><a href="#">Image</a></p>
+                                    <div class="clearfix"></div>
+                                    <hr>
+                                    Description
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                    <!--/row-->
+
+                    <div class="row">
+                        <div class="col-sm-6">
+                            <a href="#">Twitter</a> <small class="text-muted">|</small> <a href="#">Facebook</a> <small class="text-muted">|</small> <a href="#">Google+</a>
+                        </div>
+                    </div>
+
+                    <div class="row" id="footer">
+                        <div class="col-sm-6">
+
+                        </div>
+                        <div class="col-sm-6">
+                            <p>
+                                <a href="#" class="pull-right">&copy;Copyright 2022</a>
+                            </p>
+                        </div>
+                    </div>
+
+                </div><!-- /col-9 -->
+            </div><!-- /padding -->
+        </div>
+        <!-- /main -->
+    </div>
+
+
+    <!--post modal-->
+    <div id="postModal" class="modal fade" tabindex="-1" role="dialog" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">�</button>
+                    Update Status
+                </div>
+                <div class="modal-body">
+                    <form class="form center-block">
+                        <div class="form-group">
+                            <textarea class="form-control input-lg" autofocus="" placeholder="What do you want to share?"></textarea>
+                        </div>
+                    </form>
+                </div>
+                <div class="modal-footer">
+                    <div>
+                        <button class="btn btn-primary btn-sm" data-dismiss="modal" aria-hidden="true">Post</button>
+                        <ul class="pull-left list-inline">
+                            <li><a href=""></a></li>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
 </body>
+
 </html>
