7254d5615c44ace4d87a49b6a6ca656f687419f0
Pikasha08 <79849695+Pikasha08@users.noreply.github.com>
2022-04-06 08:16:10 +0200

    Mise à jour index

--- index.php
+++ index.php
@@ -1,196 +1,189 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
+require './db/posts.php';
 
-<head>
-    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
-    <meta charset="utf-8">
-    <title>Portfolio</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-    <link href="style/bootstrap.css" rel="stylesheet">
-    <link href="style/facebook.css" rel="stylesheet">
-</head>
+try {
+    if (isset($_POST['Post']))
+    {
+        for ($i = 0; $i < sizeof($_FILES['userfile']['error']); $i++)
+        {
+            if ($_FILES['userfile']['error'][$i] != 0) {
+                throw (new Exception("Il y a un erreur avec le fichier"));
+            }
+        }
+
+        if (!isset($_FILES['userfile']) ||
+        !is_uploaded_file($_FILES['userfile']['tmp_name'][0])) {
+            throw (new Exception("Probleme de transfert"));
+        }
+        else
+        {
+            $srcs = array();
+
+            $numberOfFiles = sizeof($_FILES['userfile']['name']);
+            for ($i = 0; $i < $numberOfFiles; $i++)
+            {
+                $data = file_get_contents($_FILES['userfile']['tmp_name'][$i]);
+                $mime = $_FILES['userfile']['type'][$i];
+
+                array_push($srcs, 'data:' . $mime . ';base64,' . base64_encode($data));
+            }
+
+            $_SESSION['email'] = 'sashawrc2004@gmail.com';
+            if (isset($_SESSION['email']))
+            {
+                $commentaire = filter_input(INPUT_POST, 'Commentaire', FILTER_SANITIZE_STRING);
+
+                addPost($_SESSION['email'], $commentaire, $srcs);
+            }
+        }
+    }
+}
+catch (Exception $e) {
+    echo $e->getMessage();
+}
+
+
+if (!empty($_GET['user'])) {
+    $email = filter_input(INPUT_GET, 'user', FILTER_SANITIZE_STRING);
+    $posts = getAllPostsFrom($email);
+}
+else {
+    $posts = getAllPosts();
+}
+
+include './models/header.php';
+?>
 
 <body>
+    <!-- top nav -->
+    <?php include './models/nav.php'; ?>
+    <!-- /top nav -->
 
-    <div class="wrapper">
-        <!-- main right col -->
-        <div class="column col-sm-10 col-xs-11" id="main">
-            <!-- top nav -->
-            <div class="navbar navbar-blue navbar-static-top">
-                <div class="navbar-header">
-                    <button class="navbar-toggle" type="button" data-toggle="collapse" data-target=".navbar-collapse">
-                        <span class="sr-only">Toggle</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-                    <a href="#" class="navbar-brand logo">b</a>
-                </div>
-                <nav class="collapse navbar-collapse" role="navigation">
-                    <form class="navbar-form navbar-left">
-                        <div class="input-group input-group-sm" style="max-width:360px;">
-                            <input class="form-control" placeholder="Search" name="srch-term" id="srch-term" type="text">
-                            <div class="input-group-btn">
-                                <button class="btn btn-default" type="submit"></button>
-                            </div>
-                        </div>
-                    </form>
-                    <ul class="nav navbar-nav">
-                        <li>
-                            <a href="#">Home</a>
-                        </li>
-                        <li>
-                            <a href="post.php" role="button" data-toggle="modal">Post</a>
-                        </li>
-                        <li>
-                            <a href="#"><span class="badge">badge</span></a>
-                        </li>
-                    </ul>
-                    <ul class="nav navbar-nav navbar-right">
-                        <li class="dropdown">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown"></a>
-                            <ul class="dropdown-menu">
-                                <li><a href="">More</a></li>
-                                <li><a href="">More</a></li>
-                                <li><a href="">More</a></li>
-                                <li><a href="">More</a></li>
-                                <li><a href="">More</a></li>
-                            </ul>
-                        </li>
-                    </ul>
-                </nav>
-            </div>
-            <!-- /top nav -->
-
-            <div class="padding">
-                <div class="full col-sm-9">
-                    <!-- content -->
-                    <div class="row">
-                        <!-- main col left -->
-                        <div class="col-sm-5">
-                            <div class="panel panel-default">
-                                <div class="panel-thumbnail"><img src="img/Ussr.png" class="img-responsive"></div>
-                                <div class="panel-body">
-                                    <p class="lead">Motherland</p>
-                                    <p>69 Followers, 1256 Posts</p>
-                                    <p>
-                                        <img src="img/uFp_tsTJboUY7kue5XAsGAs28.png" height="28px" width="28px">
-                                    </p>
-                                </div>
-                            </div>
-
-                            <div class="well">
-                                <form class="form-horizontal" role="form">
-                                    <h4>What's New</h4>
-                                    <div class="form-group" style="padding:14px;">
-                                        <textarea class="form-control" placeholder="Update your status"></textarea>
-                                    </div>
-                                    <button class="btn btn-primary pull-right" type="button">Post</button>
-                                    <ul class="list-inline">
-                                        <li><a href=""></a></li>
-                                    </ul>
-                                </form>
-                            </div>
-
-                            <div class="panel panel-default">
-                                <div class="panel-heading"><h4>Title</h4></div>
-                                <div class="panel-body">
-                                <p><img src="img/150x150.gif" class="img-circle pull-right"><a href="#">Image</a></p>
-                                <div class="clearfix"></div>
-                                <hr>
-                                Description
-                                </div>
-                            </div>
-
-                            <div class="panel panel-default">
-                                <div class="panel-heading"><h4>Title</h4></div>
-                                <div class="panel-body">
-                                <p><img src="img/150x150.gif" class="img-circle pull-right"><a href="#">Image</a></p>
-                                <div class="clearfix"></div>
-                                <hr>
-                                Description
-                                </div>
-                            </div>
-                        </div>
-
-                        <!-- main col right -->
-                        <div class="col-sm-7">
-
-                            <div class="panel panel-default">
-                                <div class="panel-heading">
-                                    <h4>Title</h4>
-                                </div>
-                                <div class="panel-body">
-                                    <p><img src="img/150x150.gif" class="img-circle pull-right"><a href="#">Image</a></p>
-                                    <div class="clearfix"></div>
-                                    <hr>
-                                    Description
-                                </div>
-                            </div>
-
-                            <div class="panel panel-default">
-                                <div class="panel-heading">
-                                    <h4>Title</h4>
-                                </div>
-                                <div class="panel-body">
-                                    <p><img src="img/150x150.gif" class="img-circle pull-right"><a href="#">Image</a></p>
-                                    <div class="clearfix"></div>
-                                    <hr>
-                                    Description
-                                </div>
-                            </div>
-
-                            <div class="panel panel-default">
-                                <div class="panel-heading">
-                                    <h4>Title</h4>
-                                </div>
-                                <div class="panel-body">
-                                    <p><img src="img/150x150.gif" class="img-circle pull-right"><a href="#">Image</a></p>
-                                    <div class="clearfix"></div>
-                                    <hr>
-                                    Description
-                                </div>
-                            </div>
-
-                            <div class="panel panel-default">
-                                <div class="panel-heading">
-                                    <h4>Title</h4>
-                                </div>
-                                <div class="panel-body">
-                                    <p><img src="img/150x150.gif" class="img-circle pull-right"><a href="#">Image</a></p>
-                                    <div class="clearfix"></div>
-                                    <hr>
-                                    Description
-                                </div>
-                            </div>
-
-                        </div>
-                    </div>
-                    <!--/row-->
-
-                    <div class="row">
-                        <div class="col-sm-6">
-                            <a href="#">Twitter</a> <small class="text-muted">|</small> <a href="#">Facebook</a> <small class="text-muted">|</small> <a href="#">Google+</a>
-                        </div>
-                    </div>
-
-                    <div class="row" id="footer">
-                        <div class="col-sm-6">
-
-                        </div>
-                        <div class="col-sm-6">
+    <div class="padding">
+        <div class="full col-sm-9">
+            <!-- content -->
+            <div class="row">
+                <!-- main col left -->
+                <div class="col-sm-5">
+                    <div class="panel panel-default">
+                        <div class="panel-thumbnail"><img src="img/Ussr.png" class="img-responsive"></div>
+                        <div class="panel-body">
+                            <p class="lead">Motherland</p>
+                            <p>69 Followers, 1256 Posts</p>
                             <p>
-                                <a href="#" class="pull-right">&copy;Copyright 2022</a>
+                                <img src="img/uFp_tsTJboUY7kue5XAsGAs28.png" height="28px" width="28px">
                             </p>
                         </div>
                     </div>
 
-                </div><!-- /col-9 -->
-            </div><!-- /padding -->
-        </div>
-        <!-- /main -->
-    </div>
+                    <?php include './models/addPostForm.php'; ?>
+                </div>
 
+                <!-- main col right -->
+                <div class="col-sm-7">
+                    <?php foreach ($posts as $post) { ?>
+                        <div class="panel panel-default">
+                            <div class="panel-heading">
+                                <h4><a href="index.php?user=<?= $post['nickname'] ?>"> <?= $post['nickname'] ?></a><div style="font-size: smaller; display: inline-block; padding-left: 10px">il y a <?php
+                                    $interval = (new DateTime())->diff(new DateTime($post['dateCrea']));
+                                    if ($interval->format('%a') > 0) {
+                                        echo $interval->format('%a j');
+                                    }
+                                    else if ($interval->format('%h') > 0) {
+                                        echo $interval->format('%h h');
+                                    }
+                                    else if ($interval->format('%i') > 0) {
+                                        echo $interval->format('%i m');
+                                    }
+                                    else {
+                                        echo $interval->format('%s s');
+                                    }
+                                ?></div></h4>
+                            </div>
+                            <div class="panel-body">
+                                <p><img src="<?php if (empty($post['avatar'])) { echo './img/150x150.gif'; } else { echo $post['avatar']; } ?>" class="img-circle pull-right"></p>
+                                <div class="clearfix">
+                                    <?= $post['comm'] ?>
+                                </div>
+                                <hr>
+                                <?php
+                                $imagesBalises = "";
+
+                                if (is_array($post['dataMedia']))
+                                {
+                                    $sizeOfMedia = sizeof($post['dataMedia']);
+
+                                    for ($i = 0; $i < $sizeOfMedia; $i++) {
+                                        $type = substr($post['dataMedia'][$i]['dataMedia'], 5, 10);
+
+                                       if (strpos($type, 'image') !== false) {
+                                        $imagesBalises .= '<img src="' . $post['dataMedia'] . '" width="200px">';
+                                        }
+                                        else if (strpos($type, 'audio') !== false) {
+                                            $imagesBalises .= '<audio src="' . $post['dataMedia'] . '" width="200px">';
+                                        }
+                                        else if (strpos($type, 'video') !== false) {
+                                            $imagesBalises .= '<video width="200px" controls autoplay muted loop>';
+                                            $imagesBalises .= '<source src="' . $post['dataMedia'][$i]['dataMedia'] . '"></video>';
+                                        }
+                                    }
+                                }
+                                else
+                                {
+                                    $type = substr($post['dataMedia'], 5, 10);
+
+                                    if (strpos($type, "image") !== false) {
+                                        $imagesBalises .= '<img src="' . $post['dataMedia'] . '" width="200px">';
+                                    }
+                                    else if (strpos($type, "audio") !== false) {
+                                        $imagesBalises .= '<audio src="' . $post['dataMedia'] . '" width="200px">';
+                                    }
+                                    else if (strpos($type, "video") !== false) {
+                                        $imagesBalises .= '<video width="200px" controls autoplay muted loop>';
+                                        $imagesBalises .= '<source src="' . $post['dataMedia'] . '"></video>';
+                                    }
+                                }
+
+                                echo $imagesBalises;
+                                ?>
+                                <hr>
+                                <button data-toggle="modal" class="btn btn-default btn-sm" href="#delete<?php echo $post['idPost'] ?>">
+                                    <span class="bi bi-trash"></span></button>
+
+                                    <div class="modal" id="delete<?php echo $post['idPost'] ?>">
+                                        <div class="modal-dialog">
+                                            <div class="modal-content">
+                                                <div class="modal-header">
+                                                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                                                    <h4 class="modal-title">Voulez-vous vraiment supprimer ce post ?</h4>
+                                                </div>
+                                                <div class="modal-body">
+                                                    <p><?php echo $post['comm']; ?> :
+                                                    </p>
+                                                </div>
+                                                <div class="modal-footer">
+                                                    <form action="./deletepost.php" method="post">
+                                                        <input type="hidden" name="idPost" value="<?php echo $post['idPost'] ?>" />
+                                                        <input class="btn" type="submit" name="submit" value="Supprimer" />
+                                                    </form>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <a class="btn btn-default btn-sm" href="editpost.php?id=<?= $post['idPost'] ?>">
+                                <span class="bi bi-pen"></span></a>
+                            </div>
+                        </div>
+                    <?php } ?>
+                </div>
+            </div>
+            <!--/row-->
+        </div>
+        <!-- /col-9 -->
+    </div>
+    <!-- /padding -->
+
+    <?php include './models/footer.php'; ?>
 
     <!--post modal-->
     <div id="postModal" class="modal fade" tabindex="-1" role="dialog" aria-hidden="true">
@@ -220,6 +213,7 @@
         </div>
     </div>
 
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
 </body>
-
 </html>
